b5d780f4d0d29a909d86afa64625362c0bb96565
Olufemi Peter Onafuye <162372970+pharmonny@users.noreply.github.com>
2024-11-06 22:55:41 -0500

    Add files via upload

---
+++ system_info.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "------here are the system information---------"
+uname -r
+uname -n
+uname -i
+cat /etc/redhat-release
+uptime
+hostname
+hostname -i
+last reboot
+date
+cal
+w
+whoami
+echo " those are system info"
+f1
